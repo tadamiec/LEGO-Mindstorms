8195abdda6579c3f55a95c45e44ec2a15b4b1964
tadamiec <adamiec.tomas@gmail.com>
2014-01-08 12:28:47 +0100

    working sensor

--- NXTest/src/Test.java
+++ NXTest/src/Test.java
@@ -4,35 +4,39 @@ import lejos.robotics.objectdetection.*;
 public class Test implements FeatureListener {
 	public static int MAX_DETECT = 80;
 
-    public static void main(String[] args) throws Exception{
-    	
+	public static void main(String[] args) throws Exception {
+
 		Test listener = new Test();
 		UltrasonicSensor us = new UltrasonicSensor(SensorPort.S4);
 		RangeFeatureDetector fd = new RangeFeatureDetector(us, MAX_DETECT, 500);
 		fd.addListener(listener);
-		
-		Button.ENTER.waitForPressAndRelease();
-		
-		if(us.getRange() < 50){
 
-			Motor.A.forward();
-			Motor.B.forward();
-			
-		}else{
-		
-		Motor.A.stop();
-		Motor.B.stop();
+		// Button.ENTER.waitForPressAndRelease();
+
+		Motor.A.forward();
+		Motor.B.forward();
+
+		while (true) {
+
+			if (us.getDistance() < 50) {
+
+				Motor.A.forward();
+				Motor.B.forward();
+
+			} else {
+
+				Motor.A.stop();
+				Motor.B.stop();
+			}
 		}
-		Button.waitForAnyPress();
-		
-    }
-    
+		//Button.waitForAnyPress();
+
+	}
+
 	@Override
 	public void featureDetected(Feature feature, FeatureDetector detector) {
-		int range = (int)feature.getRangeReading().getRange();
+		int range = (int) feature.getRangeReading().getRange();
 		System.out.println("Range:" + range);
 
-		
-		
 	}
 }
