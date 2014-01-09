ac7fdf14df5623eb948ebe9da645fd128a453a34
LLeGoff <lenilegoff@gmail.com>
2014-01-09 12:42:57 +0100

    Modified Follow the Wall

--- FollowTheWall/src/Main.java
+++ FollowTheWall/src/Main.java
@@ -15,7 +15,7 @@ public class Main {
 			Motor.B.rotateTo(0);
 				}
 		if(TSleft.isPressed() && !TSright.isPressed()){
-			Motor.B.rotateTo(-20);
+			Motor.B.rotateTo(0);
 			Motor.A.rotate(-360);
 			Motor.B.rotateTo(20);
 			Motor.A.rotate(360);
@@ -36,26 +36,46 @@ public class Main {
 		}
 
 	}	
+	
 	public static void main(String[] args) {
 		UltrasonicSensor us = new UltrasonicSensor(SensorPort.S4);
 		TouchSensor TSright = new TouchSensor(SensorPort.S1);
 		TouchSensor TSleft  = new TouchSensor(SensorPort.S2);
 		Motor.C.rotateTo(90);
-
+//		float alpha = 3/5;
+		int d1 = 13;
+		int d2 = 20;
+		Motor.A.setSpeed(360);
 		while(!Button.ESCAPE.isDown()){
 			
 			
-			if( us.getDistance() < 30 ){ 
+			if( us.getDistance() < d2 && us.getDistance() > d1){ 
 				turn(TSright,TSleft,us);
 				
 				Motor.B.rotateTo(0);
 				Motor.A.forward();
 			}
-			else{ //if (us.getDistance() > 20 && us.getDistance() <=40){ 
+			else if (us.getDistance() > d2) {
 				turn(TSright,TSleft,us);
-				Motor.B.rotateTo(20);
-				Motor.A.rotate(360);
+				
+				System.out.println((us.getDistance()-d2));
+				Motor.B.rotateTo(4*(us.getDistance()-d2));
+				
+				Motor.A.forward();
+
 			}
+			else if (us.getDistance() < d1){
+				turn(TSright,TSleft,us);
+				System.out.println((us.getDistance()-d1));
+
+				Motor.B.rotateTo(4*(us.getDistance() - d1));
+				Motor.A.forward();
+			}
+//			else if (us.getDistance() > d2){ //if (us.getDistance() > 20 && us.getDistance() <=40){ 
+//				turn(TSright,TSleft,us);
+//				Motor.B.rotateTo(20);
+//				Motor.A.rotate(360);
+//			}
 //			else { 
 //				Motor.B.rotateTo(0);
 //				Motor.A.forward();
