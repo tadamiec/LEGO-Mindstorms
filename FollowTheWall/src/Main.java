07a2cb80c7f902b9d123017708cde5b44ecfad66
LLeGoff <lenilegoff@gmail.com>
2014-01-09 13:42:03 +0100

    ok

--- FollowTheWall/src/Main.java
+++ FollowTheWall/src/Main.java
@@ -1,6 +1,8 @@
 import lejos.nxt.*;
 import lejos.robotics.objectdetection.RangeFeatureDetector;
 import lejos.util.Delay;
+import java.util.*;
+
 
 public class Main {
 	
@@ -9,29 +11,49 @@ public class Main {
 	 * @param args
 	 */
 	public static void turn(TouchSensor TSright ,TouchSensor TSleft,UltrasonicSensor us){	
+//		int d1 = 13;
+//		int d2 = 20;
+//		
+		Delay timer = new Delay();
+		
 		if(TSright.isPressed() && !TSleft.isPressed() ){
-			Motor.B.rotateTo(20);
+			Motor.B.rotateTo(30);
 			Motor.A.rotate(-360);
 			Motor.B.rotateTo(0);
 				}
 		if(TSleft.isPressed() && !TSright.isPressed()){
 			Motor.B.rotateTo(0);
 			Motor.A.rotate(-360);
-			Motor.B.rotateTo(20);
+			Motor.B.rotateTo(30);
 			Motor.A.rotate(360);
 				}
 		
 		if(TSright.isPressed() && TSleft.isPressed()){
 			Motor.B.rotateTo(0);
 
-			Motor.A.rotate(-360);
+			Motor.A.rotate(-90);
 			
 			Motor.C.rotateTo(-90);
 			while(us.getDistance() < 30){
+//				if( us.getDistance() < d2 && us.getDistance() > d1){ 
+//					System.out.println("Gerade aus !!!");
+//					Motor.B.rotateTo(0);
+//				}
+//				else if (us.getDistance() > d2) {								
+//					Motor.B.rotateTo(Math.max(4*(d2- us.getDistance()),-30 ));
+//				}
+//				else if (us.getDistance() < d1){
+//
+//					Motor.B.rotateTo(Math.min(4*(d1 - us.getDistance()),30) );
+//				}
 				Motor.A.backward();
 			}
+			Motor.A.stop();
+			timer.msDelay(500);
+			Motor.A.rotate(-540);
+
 			Motor.C.rotateTo(90);
-			Motor.B.rotateTo(-20);
+			Motor.B.rotateTo(-30);
 			Motor.A.rotate(720);
 		}
 
@@ -47,30 +69,29 @@ public class Main {
 		int d2 = 20;
 		Motor.A.setSpeed(360);
 		while(!Button.ESCAPE.isDown()){
-			Motor.B.rotateTo(0);
+			
 
 
 			if( us.getDistance() < d2 && us.getDistance() > d1){ 
 				turn(TSright,TSleft,us);
 				System.out.println("Gerade aus !!!");
-
-				Motor.A.forward();
+				Motor.B.rotateTo(0);
 			}
 			else if (us.getDistance() > d2) {
 				turn(TSright,TSleft,us);
 				
-				System.out.println((us.getDistance()-d2));
-				Motor.B.rotateTo(4*(us.getDistance()-d2));
+//				System.out.println((us.getDistance()-d2));
 				
-				Motor.A.forward();
+				
+				Motor.B.rotateTo(Math.min(4*(us.getDistance()-d2),30 ));
+				
 
 			}
 			else if (us.getDistance() < d1){
 				turn(TSright,TSleft,us);
-				System.out.println((us.getDistance()-d1));
+//				System.out.println((us.getDistance()-d1));
 
-				Motor.B.rotateTo(4*(us.getDistance() - d1));
-				Motor.A.forward();
+				Motor.B.rotateTo(Math.max(4*(us.getDistance()-d1),-30) );
 			}
 //			else if (us.getDistance() > d2){ //if (us.getDistance() > 20 && us.getDistance() <=40){ 
 //				turn(TSright,TSleft,us);
@@ -81,7 +102,7 @@ public class Main {
 //				Motor.B.rotateTo(0);
 //				Motor.A.forward();
 //			}
-		
+			Motor.A.forward();
 	
 	}
 }
