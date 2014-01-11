39765e51af4684c587bf488c87d04734ea17eed5
LLeGoff <lenilegoff@gmail.com>
2014-01-11 18:03:46 +0100

    yo

--- IARobot/src/Main.java
+++ IARobot/src/Main.java
@@ -2,7 +2,7 @@ import lejos.nxt.*;
 import lejos.robotics.subsumption.*;
 
 public class Main {
-
+	public static int level = 0;
 	/**
 	 * @param args
 	 */
@@ -22,28 +22,19 @@ public class Main {
 		Behavior CF = new CollisionFront(SPort4, SPort1, SPort2); // (UltraS,TSRight,TSLeft)
 		Behavior CL = new CollisionLeft(SPort1, SPort2); // (TSRight,TSLeft)
 		Behavior CR = new CollisionRight(SPort1, SPort2); // (TSRight,TSLeft)
-<<<<<<< HEAD
 		Behavior FTW = new FollowTheWall(SPort4,SPort3, d1, d2, d3,Dark,Light); // (UltraS,LightS,..)
-		Behavior SL = new SearchLabyrinthe(SPort3, Dark, Light); // (LightS,..)
+//		Behavior SL = new SearchLabyrinthe(SPort3, Dark, Light); // (LightS,..)
 //		Behavior FL = new FollowTheLine(SPort3, Dark, Light);
 //		Behavior FB = new FollowTheBridge(SPort3, Dark, Light);
 		// Behavior LSC = new LightSensorCalibration(SPort3); //(LightS)
 		Behavior StL = new StartLabyrinth(SPort3, Dark, Light); //(LightS,..)
+		Behavior TT = new TurnTable(SPort1,SPort2); //(TSRight,TSLeft)
 		
 		Behavior [] BArray = { FTW, StL, CR, CL, CF };
 		
-		
+//		Behavior [] BArray = {TT};
 
 //		Behavior[] BArray = { FB, FL };
-=======
-		//Behavior FTW = new FollowTheWall(SPort4, d1, d2); // (UltraS,..)
-		Behavior SL = new SearchLabyrinthe(SPort3, Dark, Light); // (LightS)
-		Behavior FL = new FollowTheLine(SPort3, Dark, Light);
-		Behavior FB = new FollowTheBridge(SPort3, Dark, Light);
-		// Behavior LSC = new LightSensorCalibration(SPort3); //(LightS)
-
-		Behavior[] BArray = { /*FTW, SL, CR, CL, CF,*/ FL, FB};
->>>>>>> 6f740aae23263bb2fc30b31c7ef58f8002e00080
 
 		Arbitrator Labyrinthe = new Arbitrator(BArray);
 
