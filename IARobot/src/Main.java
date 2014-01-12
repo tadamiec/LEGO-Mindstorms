0938c874e190749657f3ede580687e566ed43a65
Michael <michaelkaufen@gmail.com>
2014-01-12 18:58:11 +0100

    added the module for the gate1

--- IARobot/src/Main.java
+++ IARobot/src/Main.java
@@ -3,6 +3,8 @@ import lejos.robotics.subsumption.*;
 
 public class Main {
 	public static int level = 0;
+	public static boolean door = false;
+	public static boolean doorBumped = false;
 	/**
 	 * @param args
 	 */
@@ -10,29 +12,29 @@ public class Main {
 
 		SensorPort SPort1 = SensorPort.S1; // TouchSensor Right
 		SensorPort SPort2 = SensorPort.S2; // TouchSensor Left
-//		SensorPort SPort3 = SensorPort.S3; // LightSensor
-//		SensorPort SPort4 = SensorPort.S4; // UltraSonicSensor
-//
-//		int d1 = 15;
-//		int d2 = 18;
-//		int d3 = 22;
-//		int Dark = 21;
-//		int Light = 70;
-//
-//		Behavior CF = new CollisionFront(SPort4, SPort1, SPort2); // (UltraS,TSRight,TSLeft)
+		SensorPort SPort3 = SensorPort.S3; // LightSensor
+		SensorPort SPort4 = SensorPort.S4; // UltraSonicSensor
+
+		int d1 = 15;
+		int d2 = 18;
+		int d3 = 22;
+		int Dark = 21;
+		int Light = 70;
+
+		Behavior CF = new CollisionFront(SPort4, SPort1, SPort2); // (UltraS,TSRight,TSLeft)
 //		Behavior CL = new CollisionLeft(SPort1, SPort2); // (TSRight,TSLeft)
 //		Behavior CR = new CollisionRight(SPort1, SPort2); // (TSRight,TSLeft)
 //		Behavior FTW = new FollowTheWall(SPort4,SPort3, d1, d2, d3,Dark,Light); // (UltraS,LightS,..)
 //		Behavior SL = new SearchLabyrinthe(SPort3, Dark, Light); // (LightS,..)
 //		Behavior FL = new FollowTheLine(SPort3, Dark, Light);
 //		Behavior FB = new FollowTheBridge(SPort3, Dark, Light);
-		// Behavior LSC = new LightSensorCalibration(SPort3); //(LightS)
+//		 Behavior LSC = new LightSensorCalibration(SPort3); //(LightS)
 //		Behavior StL = new StartLabyrinth(SPort3, Dark, Light); //(LightS,..)
-		Behavior TT = new TurnTable(SPort1,SPort2); //(TSRight,TSLeft)
-		Behavior IB = new InitBluetooth();
-//		Behavior [] BArray = { FTW, StL, CR, CL, CF };
+//		Behavior TT = new TurnTable(SPort1,SPort2); //(TSRight,TSLeft)
+//		Behavior IB = new InitBluetooth();
+		Behavior OD = new OpenADoor();
 		
-		Behavior [] BArray = {IB,TT};
+		Behavior [] BArray = {OD,CF};
 
 //		Behavior[] BArray = { FB};
 		
