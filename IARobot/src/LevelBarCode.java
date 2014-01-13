5f5650a33aa4114709a6a7cc82a9a45f49a387e8
LLeGoff <lenilegoff@gmail.com>
2014-01-13 16:50:20 +0100

    Modified : Collisions, Follow the wall

--- IARobot/src/LevelBarCode.java
+++ IARobot/src/LevelBarCode.java
@@ -23,12 +23,12 @@ public class LevelBarCode implements Behavior{
 		boolean wasWhite = false;
 		boolean wasBlack = true;
 		
-		Motor.B.rotateTo(0);
+//		Motor.B.rotateTo(0);
 		boolean EndBarcode = false;
 		long Time = System.currentTimeMillis();
 		
 		while(!EndBarcode && !suppressed){
-			Motor.A.rotate(20);
+			Main.pilot.forward();
 			if((wasWhite && ls.getLightValue() < 1100)){
 				wasWhite = false;
 				wasBlack = true;
--- IARobot/src/Main.java
+++ IARobot/src/Main.java
@@ -7,17 +7,19 @@ public class Main {
 
 	public static boolean door = false;
 	public static boolean doorBumped = false;
-
+	public static DifferentialPilot pilot = new DifferentialPilot(32, 120 , Motor.A, Motor.B);
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
+		
+		
 		SensorPort SPort1 = SensorPort.S1; // TouchSensor Right
 		SensorPort SPort2 = SensorPort.S2; // TouchSensor Left
 		SensorPort SPort3 = SensorPort.S3; // LightSensor
 		SensorPort SPort4 = SensorPort.S4; // UltraSonicSensor
-<<<<<<< HEAD
+
 //		int d1 = 15;
 //		int d2 = 18;
 //		int d3 = 22;
@@ -31,41 +33,17 @@ public class Main {
 //		Behavior SL = new SearchLabyrinthe(SPort3, Dark, Light); // (LightS,..)
 //		Behavior FL = new FollowTheLine(SPort3, Dark, Light);
 //		Behavior FB = new FollowTheBridge(SPort3, Dark, Light);
-		// Behavior LSC = new LightSensorCalibration(SPort3); //(LightS)
+//      Behavior LSC = new LightSensorCalibration(SPort3); //(LightS)
 //		Behavior StL = new StartLabyrinth(SPort3, Dark, Light); //(LightS,..)
 		Behavior TT = new TurnTable(SPort1,SPort2); //(TSRight,TSLeft)
-		Behavior IB = new InitBluetooth();
-		Behavior S = new Schneller();
-//		Behavior [] BArray = { FTW, StL, CR, CL, CF };
+		Behavior Test = new DiffPilotMotorTest();
 		
-		Behavior [] BArray = {S,CL,CR,CF};
+		Behavior [] BArray = {Test};
 
 //		Behavior[] BArray = { FB};
 		
-=======
 
-		int d1 = 15;
-		int d2 = 18;
-		int d3 = 22;
-		int Dark = 21;
-		int Light = 70;
 
-		Behavior CF = new CollisionFront(SPort4, SPort1, SPort2); // (UltraS,TSRight,TSLeft)
-		 Behavior CL = new CollisionLeft(SPort1, SPort2); // (TSRight,TSLeft)
-		 Behavior CR = new CollisionRight(SPort1, SPort2); // (TSRight,TSLeft)
-		 Behavior FTW = new FollowTheWall(SPort4,SPort3, d1, d2, d3,Dark,Light); // (UltraS,LightS,..)
-		// Behavior SL = new SearchLabyrinthe(SPort3, Dark, Light); //
-		// (LightS,..)
-		Behavior FL = new FollowTheLine(SPort3, Dark, Light);
-		// Behavior FB = new FollowTheBridge(SPort3, Dark, Light);
-		// Behavior LSC = new LightSensorCalibration(SPort3); //(LightS)
-		// Behavior StL = new StartLabyrinth(SPort3, Dark, Light); //(LightS,..)
-		// Behavior TT = new TurnTable(SPort1,SPort2); //(TSRight,TSLeft)
-		// Behavior IB = new InitBluetooth();
-		Behavior OD = new OpenADoor();
->>>>>>> 080f64d5437970ce446c18c6e86086f7a4d873ea
-
-		Behavior[] BArray = { /*FTW, CL, CR, CF*/ FL};
 
 		Arbitrator Labyrinthe = new Arbitrator(BArray);
 
