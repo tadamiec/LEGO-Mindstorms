23c61af857b5cb736fdbb19f0761320bd91ef152
tadamiec <adamiec.tomas@gmail.com>
2014-01-13 10:44:42 +0100

    main wachseln

--- IARobot/src/Main.java
+++ IARobot/src/Main.java
@@ -3,6 +3,7 @@ import lejos.robotics.subsumption.*;
 
 public class Main {
 	public static int level = 0;
+
 	/**
 	 * @param args
 	 */
@@ -18,30 +19,22 @@ public class Main {
 		int d3 = 22;
 		int Dark = 21;
 		int Light = 70;
-//
-//		Behavior CF = new CollisionFront(SPort4, SPort1, SPort2); // (UltraS,TSRight,TSLeft)
-//		Behavior CL = new CollisionLeft(SPort1, SPort2); // (TSRight,TSLeft)
-//		Behavior CR = new CollisionRight(SPort1, SPort2); // (TSRight,TSLeft)
-//		Behavior FTW = new FollowTheWall(SPort4,SPort3, d1, d2, d3,Dark,Light); // (UltraS,LightS,..)
-//		Behavior SL = new SearchLabyrinthe(SPort3, Dark, Light); // (LightS,..)
-//		Behavior FL = new FollowTheLine(SPort3, Dark, Light);
+		//
+		// Behavior CF = new CollisionFront(SPort4, SPort1, SPort2); //
+		// (UltraS,TSRight,TSLeft)
+		// Behavior CL = new CollisionLeft(SPort1, SPort2); // (TSRight,TSLeft)
+		// Behavior CR = new CollisionRight(SPort1, SPort2); // (TSRight,TSLeft)
+		// Behavior FTW = new FollowTheWall(SPort4,SPort3, d1, d2,
+		// d3,Dark,Light); // (UltraS,LightS,..)
+		// Behavior SL = new SearchLabyrinthe(SPort3, Dark, Light); //
+		// (LightS,..)
+		Behavior FL = new FollowTheLine(SPort3, Dark, Light);
 		Behavior FB = new FollowTheBridge(SPort3, Dark, Light);
 		// Behavior LSC = new LightSensorCalibration(SPort3); //(LightS)
-//		Behavior StL = new StartLabyrinth(SPort3, Dark, Light); //(LightS,..)
-//		Behavior TT = new TurnTable(SPort1,SPort2); //(TSRight,TSLeft)
-		
-<<<<<<< HEAD
-//		Behavior [] BArray = { FTW, StL, CR, CL, CF };
-		
-//		Behavior [] BArray = {TT};
+		// Behavior StL = new StartLabyrinth(SPort3, Dark, Light); //(LightS,..)
+		// Behavior TT = new TurnTable(SPort1,SPort2); //(TSRight,TSLeft)
 
-		Behavior[] BArray = { FB};
-=======
-		
-//		Behavior [] BArray = {TT};
-
-		Behavior[] BArray = { /*FTW, SL, CR, CL, CF,*/ FL/*, FB*/};
->>>>>>> cf835723f2a58dbce07d74070462ea33e6174d3b
+		Behavior[] BArray = { /* FTW, SL, CR, CL, CF, */FL /* , FB */};
 
 		Arbitrator Labyrinthe = new Arbitrator(BArray);
 
