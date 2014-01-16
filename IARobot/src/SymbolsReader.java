714dc81230316563c9c2ec212e2bb6c0d1343c2e
tadamiec <adamiec.tomas@gmail.com>
2014-01-16 20:33:13 +0100

    finished Symbol

--- IARobot/src/SymbolsReader.java
+++ IARobot/src/SymbolsReader.java
@@ -3,30 +3,18 @@ import java.io.File;
 import lejos.nxt.LightSensor;
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
-import lejos.nxt.Sound;
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.subsumption.Behavior;
-
-//import lejos.nxt.Sound;
+import lejos.util.Delay;
 
 public class SymbolsReader implements Behavior {
 	private LightSensor ls;
 	private boolean suppressed = false;
-	private boolean tryLeft = false;
-	private boolean alreadyForwards = false;
-	private boolean minus = false;
-	private boolean leftChecked = false;
-	private boolean rightChecked = false;
 
-	long startTime, currentTime;
-	// static File pw = new File("power_up_8bit.wav");
+	static File pw = new File("power_up_8bit.wav");
 
 	DifferentialPilot pilot = new DifferentialPilot(30, 40, Motor.A, Motor.B,
 			true);
-
-	SymbolTravelData symbolTravelData = new SymbolTravelData();
-
-	static File pw = new File("power_up_8bit.wav");
 
 	public SymbolsReader(SensorPort LS, int Dark, int Light) {
 		ls = new LightSensor(LS);
@@ -36,141 +24,126 @@ public class SymbolsReader implements Behavior {
 
 	@Override
 	public boolean takeControl() {
-		return (ls.getLightValue() > 1400 && ls.getLightValue() < 2000);
+		return (ls.getLightValue() > 1700);
 	}
 
 	@Override
 	public void action() {
 		suppressed = false;
-		int angle = 7;
-		int limitAngle = angle;
-		// int correction = 30;
+		float incrementedAngle = 0;
+		float returnAngle = 0;
+		double storeAngle = 0;
+		int rangeAngleLeft = 10, rangeAngleRight = 10;
+		int rangeAngle = 20;
+		boolean leftChecked = false;
+		boolean rightChecked = false;
+		boolean firstIncrement = false;
 
 		while (!suppressed) {
-			leftChecked = false;
-			rightChecked = false;
-
-			while (ls.getLightValue() > 1400 && ls.getLightValue() < 2000) {
-				// startTime = System.currentTimeMillis();
+			while (ls.getLightValue() > 1700) {
+				Main.pilot.setTravelSpeed(100);
 				Main.pilot.forward();
-				// System.out.println(ls.getLightValue());
-				alreadyForwards = false;
-				// fastIncrease50 = false;
-				// fastIncrease140 = false;
-			}
-
-			while (ls.getLightValue() > 900 && ls.getLightValue() < 1400) {
-				Main.pilot.forward();
-			}
-
-			while (ls.getLightValue() > 650 && ls.getLightValue() < 900) {
-				// Move a bit forward from the line
-				// if (!alreadyForwards && Math.abs(limitAngle) > 20) {
-				// Main.pilot.forward();
-				// Delay.msDelay(300);
-				// alreadyForwards = true;
-				// }
-
-				if (tryLeft) {
-					// Main.pilot.rotate(limitAngle);
-					if (Math.abs(limitAngle) < 20) {
-						Main.pilot.rotate(limitAngle);
-						tryLeft = false;
-						// Return to previous position
-						Main.pilot.rotate(-limitAngle);	
-					} else if (Math.abs(limitAngle) > 20) {
-						Main.pilot.rotate(limitAngle);
-						leftChecked = true;
-					} else if (Math.abs(limitAngle) > 180) {
-
-						System.out.println(limitAngle);
-						Main.pilot.rotate(-limitAngle);
-						limitAngle = angle;
-						tryLeft = false;
-					} else {
-						Sound.playSample(pw, 25);
-					}
-					minus = false;
-				} else {
-					// Main.pilot.rotate(-limitAngle);
-					if (Math.abs(limitAngle) < 20) {
-						Main.pilot.rotate(-limitAngle);
-						tryLeft = true;
-						// Return to previous position
-						Main.pilot.rotate(limitAngle);
-					} else if (Math.abs(limitAngle) > 20) {
-						Main.pilot.rotate(limitAngle);
-						rightChecked = true;
-					} else if (Math.abs(limitAngle) > 180) {
-
-						System.out.println(limitAngle);
-						Main.pilot.rotate(limitAngle);
-
-						limitAngle = angle;
-						tryLeft = true;
-					} else {
-						Sound.playSample(pw, 25);
-					}
-
-					minus = true;
+				if ((rangeAngle > 20)) {
+					// System.out.println(storeAngle);
+					Main.symbolAngleList.add(storeAngle);
 				}
-
-				// Increment angle steps
-				/*
-				 * if (limitAngle > 30 && !fastIncrease50) { limitAngle = 50;
-				 * fastIncrease50 = true; } else if (limitAngle < -30 &&
-				 * !fastIncrease50) { limitAngle = -50 - correction;
-				 * fastIncrease50 = true; }
-				 * 
-				 * if (limitAngle > 90 && fastIncrease50 && !fastIncrease140) {
-				 * limitAngle = 135; fastIncrease140 = true; } else if
-				 * (limitAngle < -90 && fastIncrease50 && !fastIncrease140) {
-				 * limitAngle = -135 - correction; fastIncrease140 = true; }
-				 */
-
-				limitAngle += angle;
+				rangeAngle = 20;
+				rangeAngleLeft = 10;
+				rangeAngleRight = 10;
+				incrementedAngle = 0;
+				returnAngle = 0;
+				storeAngle = 0;
+				leftChecked = false;
+				rightChecked = false;
+				firstIncrement = false;
 			}
 
-<<<<<<< HEAD
-			// if (!minus && (limitAngle > (3 * angle))) {
-			// currentTime = System.currentTimeMillis();
-			// symbolTravelData.setAngle(limitAngle);
-			// symbolTravelData.setTime(currentTime - startTime);
-			// Main.symbolTravelDataList.add(symbolTravelData);
-			// System.out.println(limitAngle);
-			// System.out.println(currentTime - startTime);
-			// } else if (minus && (limitAngle > (3 * angle))) {
-			// currentTime = System.currentTimeMillis();
-			// symbolTravelData.setAngle(limitAngle);
-			// symbolTravelData.setTime(currentTime - startTime);
-			// Main.symbolTravelDataList.add(symbolTravelData);
-			// System.out.println(-limitAngle);
-			// System.out.println(currentTime - startTime);
-			// }
-=======
-			if (!minus && (limitAngle > (3 * angle))) {
-				currentTime = System.currentTimeMillis();
-				symbolTravelData.setAngle(limitAngle);
-				symbolTravelData.setTime(currentTime - startTime);
-//				Main.symbolTravelDataList.add(symbolTravelData);
-				System.out.println(limitAngle);
-				System.out.println(currentTime - startTime);
-			} else if (minus && (limitAngle > (3 * angle))) {
-				currentTime = System.currentTimeMillis();
-				symbolTravelData.setAngle(limitAngle);
-				symbolTravelData.setTime(currentTime - startTime);
-//				Main.symbolTravelDataList.add(symbolTravelData);
-				System.out.println(-limitAngle);
-				System.out.println(currentTime - startTime);
-			}
->>>>>>> branch 'master' of https://github.com/tadamiec/LEGO-Mindstorms.git
+			while (ls.getLightValue() < 1700) {
+				Main.pilot.setRotateSpeed(30);
+				if (!leftChecked) {
+					Main.pilot.rotateLeft();
+					incrementedAngle -= Main.pilot.getAngleIncrement();
 
-			limitAngle = angle;
+					// Get angle to store
+					storeAngle = incrementedAngle;
+
+					if (Math.abs(incrementedAngle) > rangeAngleLeft) {
+						Main.pilot.setRotateSpeed(50);
+
+						// Go Back
+						returnAngle = 0;
+						while (Math.abs(returnAngle) < Math
+								.abs(incrementedAngle)) {
+							Main.pilot.rotateRight();
+							returnAngle += Main.pilot.getAngleIncrement();
+						}
+
+						incrementedAngle = 0;
+						leftChecked = true;
+					}
+				} else if (!rightChecked && (ls.getLightValue() < 1700)) {
+					Main.pilot.rotateRight();
+					incrementedAngle += Main.pilot.getAngleIncrement();
+
+					// Get angle to store
+					storeAngle = incrementedAngle;
+
+					if (Math.abs(incrementedAngle) > rangeAngleRight) {
+						Main.pilot.setRotateSpeed(50);
+
+						// Go Back
+						returnAngle = 0;
+						while (Math.abs(returnAngle) < Math
+								.abs(incrementedAngle)) {
+							Main.pilot.rotateLeft();
+							returnAngle += Main.pilot.getAngleIncrement();
+						}
+
+						incrementedAngle = 0;
+						rightChecked = true;
+					}
+				} else if (leftChecked && rightChecked && (rangeAngle == 55)) {
+					// no line
+					Main.pilot.stop();
+					getSymbol();
+				} else if (ls.getLightValue() < 1700) {
+					Delay.msDelay(100);
+					Main.pilot.travel(25);
+					if (!firstIncrement) {
+						rangeAngle += 35;
+						firstIncrement = true;
+					}
+
+					rangeAngleLeft = rangeAngle - 5;
+					rangeAngleRight = rangeAngle + 35;
+					leftChecked = false;
+					rightChecked = false;
+				}
+			}
+
+			Thread.yield();
 		}
 	}
 
 	@Override
 	public void suppress() {
 		suppressed = true;
+	}
+
+	public void getSymbol() {
+		if (Main.symbolAngleList.size() < 2) {
+			System.out.println("L");
+		} else if (Main.symbolAngleList.size() > 2) {
+			System.out.println("M");
+		} else if ((Main.symbolAngleList.get(0) < -70)
+				&& (Main.symbolAngleList.get(1) < -65)) {
+			System.out.println("M");
+		} else if ((Main.symbolAngleList.get(0) < -70)
+				&& (Main.symbolAngleList.get(1) > -40)) {
+			System.out.println("Z");
+		} else if ((Main.symbolAngleList.get(0) > -65)
+				&& (Main.symbolAngleList.get(1) > -75)) {
+			System.out.println("U");
+		}
 	}
 }
